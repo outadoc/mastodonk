480816c07e70aaed261078d009b8f73998c1d7b3
Baptiste Candellier <outadoc@gmail.com>
2021-04-24 16:42:36 +0200

    chore(build): improve source set config

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("plugin.serialization") version "1.4.32"
 }
 
-group = "fr.outadoc"
+group = "fr.outadoc.mastodonk"
 version = "1.0"
 
 repositories {
@@ -15,19 +15,21 @@ kotlin {
 
     jvm {
         compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions {
+                useIR = true
+                jvmTarget = "1.8"
+            }
         }
+
         testRuns["test"].executionTask.configure {
             useJUnit()
         }
     }
-    js(LEGACY) {
-        browser {
-            commonWebpackConfig {
-                cssSupport.enabled = true
-            }
-        }
+
+    js(IR) {
+        nodejs()
     }
+
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
@@ -36,7 +38,6 @@ kotlin {
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
-
 
     sourceSets {
         val commonMain by getting {
@@ -51,18 +52,21 @@ kotlin {
                 implementation(kotlin("test-annotations-common"))
             }
         }
+
         val jvmMain by getting
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
             }
         }
+
         val jsMain by getting
         val jsTest by getting {
             dependencies {
                 implementation(kotlin("test-js"))
             }
         }
+
         val nativeMain by getting
         val nativeTest by getting
     }
