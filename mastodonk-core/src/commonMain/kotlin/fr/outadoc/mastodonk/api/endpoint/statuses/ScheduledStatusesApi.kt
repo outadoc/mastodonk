7fdfcaa149270ea15af91cde02e32a517d40c898
Baptiste Candellier <baptiste@candellier.me>
2021-04-27 21:00:26 +0200

    Document scheduled statuses

--- mastodonk-core/src/commonMain/kotlin/fr/outadoc/mastodonk/api/endpoint/statuses/ScheduledStatusesApi.kt
+++ mastodonk-core/src/commonMain/kotlin/fr/outadoc/mastodonk/api/endpoint/statuses/ScheduledStatusesApi.kt
@@ -3,8 +3,16 @@ package fr.outadoc.mastodonk.api.endpoint.statuses
 import fr.outadoc.mastodonk.api.entity.ScheduledStatus
 import fr.outadoc.mastodonk.api.entity.request.statuses.ScheduledStatusUpdate
 
+/**
+ * View and manage currently scheduled statuses.
+ *
+ * @see [Official Docs](https://docs.joinmastodon.org/methods/statuses/scheduled_statuses/)
+ */
 public interface ScheduledStatusesApi {
 
+    /**
+     * Gets currently scheduled statuses for this account.
+     */
     public suspend fun getScheduledStatuses(
         limit: Int? = null,
         maxId: String? = null,
@@ -12,9 +20,18 @@ public interface ScheduledStatusesApi {
         minId: String? = null
     ): List<ScheduledStatus>
 
+    /**
+     * Gets a single [ScheduledStatus].
+     */
     public suspend fun getScheduledStatus(id: String): ScheduledStatus
 
+    /**
+     * Updates the schedule of a [ScheduledStatus].
+     */
     public suspend fun updateScheduledStatus(id: String, update: ScheduledStatusUpdate): ScheduledStatus
 
+    /**
+     * Cancels a [ScheduledStatus].
+     */
     public suspend fun cancelSchedulesStatus(id: String): ScheduledStatus
 }
