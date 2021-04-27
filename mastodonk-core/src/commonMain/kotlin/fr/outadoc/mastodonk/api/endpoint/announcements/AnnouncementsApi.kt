e11ee1df3855895a6f3339089f5fde62f1cfff8a
Baptiste Candellier <baptiste@candellier.me>
2021-04-27 18:31:31 +0200

    Document Announcements api

--- mastodonk-core/src/commonMain/kotlin/fr/outadoc/mastodonk/api/endpoint/announcements/AnnouncementsApi.kt
+++ mastodonk-core/src/commonMain/kotlin/fr/outadoc/mastodonk/api/endpoint/announcements/AnnouncementsApi.kt
@@ -2,13 +2,36 @@ package fr.outadoc.mastodonk.api.endpoint.announcements
 
 import fr.outadoc.mastodonk.api.entity.Announcement
 
+/**
+ * View and manage instance-wide announcements.
+ *
+ * @see [Official Docs](https://docs.joinmastodon.org/methods/announcements/)
+ */
 public interface AnnouncementsApi {
 
+    /**
+     * See all currently active announcements set by admins.
+     *
+     * @param withDismissed Whether to include dismissed announcements.
+     */
     public suspend fun getAllAnnouncements(withDismissed: Boolean? = null): List<Announcement>
 
+    /**
+     * Dismisses an announcement.
+     */
     public suspend fun dismissAnnouncement(id: String)
 
+    /**
+     * Adds a reaction to an announcement.
+     *
+     * @param name Unicode emoji, or shortcode of custom emoji.
+     */
     public suspend fun addReaction(id: String, name: String)
 
+    /**
+     * Removes a reaction to an announcement.
+     *
+     * @param name Unicode emoji, or shortcode of custom emoji.
+     */
     public suspend fun removeReaction(id: String, name: String)
 }
