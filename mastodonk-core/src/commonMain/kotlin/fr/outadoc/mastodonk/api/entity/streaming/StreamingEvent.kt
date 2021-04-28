f07a9e1963c459df4edc5601f015f48001f66c38
Baptiste Candellier <baptiste@candellier.me>
2021-04-28 18:19:30 +0200

    Support conversation streaming event

--- mastodonk-core/src/commonMain/kotlin/fr/outadoc/mastodonk/api/entity/streaming/StreamingEvent.kt
+++ mastodonk-core/src/commonMain/kotlin/fr/outadoc/mastodonk/api/entity/streaming/StreamingEvent.kt
@@ -1,5 +1,6 @@
 package fr.outadoc.mastodonk.api.entity.streaming
 
+import fr.outadoc.mastodonk.api.entity.Conversation
 import fr.outadoc.mastodonk.api.entity.Notification
 import fr.outadoc.mastodonk.api.entity.Status
 
@@ -19,6 +20,11 @@ public data class UpdateEvent(public val payload: Status) : StreamingEvent()
  * A new notification has appeared.
  */
 public data class NotificationEvent(public val payload: Notification) : StreamingEvent()
+
+/**
+ * A conversation has been updated.
+ */
+public data class ConversationEvent(public val payload: Conversation) : StreamingEvent()
 
 /**
  * The status with the given [statusId] has been deleted.
