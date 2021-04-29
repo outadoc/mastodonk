ba67b9fe32a7707ca94bc519b9dfe037996cb91c
Baptiste Candellier <baptiste@candellier.me>
2021-04-29 21:03:10 +0200

    Setup POM & github repo

--- mastodonk-core/build.gradle.kts
+++ mastodonk-core/build.gradle.kts
@@ -33,7 +33,13 @@ kotlin {
 
     publishing {
         repositories {
-            mavenLocal()
+            maven {
+                url = uri("https://maven.pkg.github.com/outadoc/mastodonk")
+                credentials {
+                    username = System.getenv("GHP_USERNAME")
+                    password = System.getenv("GHP_TOKEN")
+                }
+            }
         }
 
         val publicationsFromMainHost = listOf(
@@ -52,6 +58,36 @@ kotlin {
                             findProperty("isMainHost") == "true"
                         }
                     }
+            }
+
+            create<MavenPublication>("maven") {
+                pom {
+                    name.set("Mastodonk")
+                    description.set("Kotlin/Multiplatform library for Mastodon")
+                    url.set("https://github.com/outadoc/mastodonk")
+
+                    issueManagement {
+                        url.set("https://github.com/outadoc/mastodonk/issues")
+                        system.set("GitHub Issues")
+                    }
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Baptiste Candellier")
+                            email.set("baptiste@candellier.me")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://github.com/outadoc/mastodonk.git")
+                        developerConnection.set("scm:git:git://github.com/outadoc/mastodonk.git")
+                        url.set("https://github.com/outadoc/mastodonk")
+                    }
+                }
             }
         }
     }
