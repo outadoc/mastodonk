25513a0ec5fa12541115598c025d582c5aec5394
Baptiste Candellier <baptiste@candellier.me>
2021-04-29 21:06:00 +0200

    feat(maven): publish packages (#15)

--- mastodonk-core/build.gradle.kts
+++ mastodonk-core/build.gradle.kts
@@ -4,10 +4,8 @@ plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization")
     id("org.jetbrains.dokka")
+    `maven-publish`
 }
-
-group = "fr.outadoc.mastodonk"
-version = "0.1-alpha"
 
 kotlin {
     explicitApi()
@@ -15,7 +13,6 @@ kotlin {
     jvm {
         compilations.all {
             kotlinOptions {
-                useIR = true
                 jvmTarget = "1.8"
             }
         }
@@ -26,19 +23,79 @@ kotlin {
     }
 
     js(IR) {
+        browser()
         nodejs()
     }
 
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    linuxX64()
+    macosX64()
+    mingwX64()
+
+    publishing {
+        repositories {
+            maven {
+                url = uri("https://maven.pkg.github.com/outadoc/mastodonk")
+                credentials {
+                    username = System.getenv("GHP_USERNAME")
+                    password = System.getenv("GHP_TOKEN")
+                }
+            }
+        }
+
+        val publicationsFromMainHost = listOf(
+            jvm().name,
+            js().name,
+            "kotlinMultiplatform"
+        )
+
+        publications {
+            matching { it.name in publicationsFromMainHost }.all {
+                val targetPublication = this@all
+                tasks.withType<AbstractPublishToMaven>()
+                    .matching { it.publication == targetPublication }
+                    .configureEach {
+                        onlyIf {
+                            findProperty("isMainHost") == "true"
+                        }
+                    }
+            }
+
+            create<MavenPublication>("maven") {
+                pom {
+                    name.set("Mastodonk")
+                    description.set("Kotlin/Multiplatform library for Mastodon")
+                    url.set("https://github.com/outadoc/mastodonk")
+
+                    issueManagement {
+                        url.set("https://github.com/outadoc/mastodonk/issues")
+                        system.set("GitHub Issues")
+                    }
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Baptiste Candellier")
+                            email.set("baptiste@candellier.me")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://github.com/outadoc/mastodonk.git")
+                        developerConnection.set("scm:git:git://github.com/outadoc/mastodonk.git")
+                        url.set("https://github.com/outadoc/mastodonk")
+                    }
+                }
+            }
+        }
     }
 
     sourceSets {
+
+        // Common source-sets
+
         val commonMain by getting {
             dependencies {
                 implementation(libs.datetime)
@@ -48,6 +105,7 @@ kotlin {
                 implementation(libs.ktor.websockets)
             }
         }
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-common"))
@@ -55,34 +113,48 @@ kotlin {
             }
         }
 
+        // JVM source-sets
+
         val jvmMain by getting {
             dependencies {
                 implementation(libs.ktor.engine.cio)
             }
         }
+
         val jvmTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
             }
         }
 
+        // JS source-sets
+
         val jsMain by getting {
             dependencies {
                 implementation(libs.ktor.engine.js)
             }
         }
+
         val jsTest by getting {
             dependencies {
                 implementation(kotlin("test-js"))
             }
         }
 
-        val nativeMain by getting {
+        // Desktop source-sets
+
+        val desktopMain by creating {
+            dependsOn(commonMain)
             dependencies {
                 implementation(libs.ktor.engine.curl)
             }
         }
-        val nativeTest by getting
+
+        val desktopTest by creating
+
+        val linuxX64Main by getting { dependsOn(desktopMain) }
+        val mingwX64Main by getting { dependsOn(desktopMain) }
+        val macosX64Main by getting { dependsOn(desktopMain) }
     }
 }
 
