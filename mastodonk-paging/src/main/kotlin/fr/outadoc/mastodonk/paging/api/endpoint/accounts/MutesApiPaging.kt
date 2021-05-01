9d7e60d9a0c8d20c03aebec62adf42d92f12c936
Baptiste Candellier <baptiste@candellier.me>
2021-05-01 20:13:11 +0200

    Add mutes api

--- mastodonk-paging/src/main/kotlin/fr/outadoc/mastodonk/paging/api/endpoint/accounts/MutesApiPaging.kt
+++ mastodonk-paging/src/main/kotlin/fr/outadoc/mastodonk/paging/api/endpoint/accounts/MutesApiPaging.kt
@@ -1,13 +1,14 @@
 package fr.outadoc.mastodonk.paging.api.endpoint.accounts
 
 import androidx.paging.PagingSource
-import fr.outadoc.mastodonk.api.endpoint.accounts.DomainBlocksApi
+import fr.outadoc.mastodonk.api.endpoint.accounts.MutesApi
+import fr.outadoc.mastodonk.api.entity.Account
 import fr.outadoc.mastodonk.api.entity.paging.PageInfo
 import fr.outadoc.mastodonk.paging.pagingSource
 
-public fun DomainBlocksApi.getDomainBlocksSource(): PagingSource<PageInfo, String> {
+public fun MutesApi.getMutesSource(): PagingSource<PageInfo, Account> {
     return pagingSource { params ->
-        getDomainBlocks(
+        getMutes(
             limit = params.loadSize,
             pageInfo = params.key
         )
