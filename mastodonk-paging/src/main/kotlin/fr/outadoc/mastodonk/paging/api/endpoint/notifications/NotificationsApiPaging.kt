e7eb97a2bda1aab16872d7b3876f25a5ea2cb286
Baptiste Candellier <baptiste@candellier.me>
2021-05-01 20:32:20 +0200

    Add some more

---
+++ mastodonk-paging/src/main/kotlin/fr/outadoc/mastodonk/paging/api/endpoint/notifications/NotificationsApiPaging.kt
@@ -0,0 +1,22 @@
+package fr.outadoc.mastodonk.paging.api.endpoint.notifications
+
+import androidx.paging.PagingSource
+import fr.outadoc.mastodonk.api.endpoint.notifications.NotificationsApi
+import fr.outadoc.mastodonk.api.entity.Notification
+import fr.outadoc.mastodonk.api.entity.NotificationType
+import fr.outadoc.mastodonk.api.entity.paging.PageInfo
+import fr.outadoc.mastodonk.paging.pagingSource
+
+public fun NotificationsApi.getNotificationsSource(
+    excludeTypes: List<NotificationType>? = null,
+    accountId: String? = null
+): PagingSource<PageInfo, Notification> {
+    return pagingSource { params ->
+        getNotifications(
+            excludeTypes = excludeTypes,
+            accountId = accountId,
+            limit = params.loadSize,
+            pageInfo = params.key
+        )
+    }
+}
