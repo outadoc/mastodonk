d3cef2f6a245f1e10f45ececae87abd107fae168
Baptiste Candellier <baptiste@candellier.me>
2021-10-11 19:35:42 +0200

    feat(accounts): support paging for GET accounts/statuses

--- mastodonk-paging/src/jvmMain/kotlin/fr/outadoc/mastodonk/paging/api/endpoint/accounts/AccountsApiPaging.kt
+++ mastodonk-paging/src/jvmMain/kotlin/fr/outadoc/mastodonk/paging/api/endpoint/accounts/AccountsApiPaging.kt
@@ -2,6 +2,7 @@ package fr.outadoc.mastodonk.paging.api.endpoint.accounts
 
 import fr.outadoc.mastodonk.api.endpoint.accounts.AccountsApi
 import fr.outadoc.mastodonk.api.entity.Account
+import fr.outadoc.mastodonk.api.entity.Status
 import fr.outadoc.mastodonk.api.entity.paging.PageInfo
 import fr.outadoc.mastodonk.paging.PagingSource
 import fr.outadoc.mastodonk.paging.pagingSource
@@ -19,6 +20,16 @@ public actual fun AccountsApi.getFollowersSource(accountId: String): PagingSourc
 public actual fun AccountsApi.getFollowingSource(accountId: String): PagingSource<PageInfo, Account> {
     return pagingSource { params ->
         getFollowing(
+            accountId = accountId,
+            limit = params.loadSize,
+            pageInfo = params.key
+        )
+    }
+}
+
+public actual fun AccountsApi.getStatusesSource(accountId: String): PagingSource<PageInfo, Status> {
+    return pagingSource { params ->
+        getStatuses(
             accountId = accountId,
             limit = params.loadSize,
             pageInfo = params.key
